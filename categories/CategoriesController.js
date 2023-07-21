83347a09410872f824ec097c8db62f4c7632a64c
Davi <88855441+davi12345452@users.noreply.github.com>
2023-07-21 09:54:47 -0300

    listando as categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -6,18 +6,22 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("./Category");
-const Slugify = require("slugify")
+const Slugify = require("slugify");
 
 // Página para a criação de categorias pelo ADM, no seu painel
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new.ejs");
 });
 
-router.post("/categories/new", (req, res) => {
+router.get("/admin/categories", (req, res) => {
+    res.render("admin/categories/index.ejs");
+})
+
+router.post("/categories/new-save", (req, res) => {
     let _title = req.body.title
     if(_title == undefined){
         console.log("Error, undefined category");
-        res.redirect("/admin/categories/new")
+        res.redirect("/admin/categories/new");
     }else{
         Category.create({
             title: _title,
