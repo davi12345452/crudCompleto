4352b9420666c715b7f8d9d7a6d5e64f0ec44f0a
Davi <88855441+davi12345452@users.noreply.github.com>
2023-07-21 16:05:52 -0300

    manipulando a api do tinymce para criar um editor de texto dentro das páginas

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,9 +5,43 @@
 
 const express = require("express");
 const router = express.Router();
+const Article = require("./Article");
+const Slugify = require("slugify");
+const { default: slugify } = require("slugify");
 
-router.get("/artigos", (req, res) => {
-    res.send("Rota artigo")
+// Rota para a página de criação de artigos
+
+router.get("/admin/articles/new", (req, res) => {
+    res.render("admin/articles/new.ejs")
+})
+
+// Rota para a página que lista os artigos
+
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({raw:true}).then(_articles => {
+        res.render("admin/articles/index.ejs", {
+            articles: _articles
+        });
+    })
+    
+})
+
+// Rota post para criar um artigo
+
+router.post("/articles/new-save", (req, res) => {
+    let _title = req.body.title;
+    let _text = req.body.text;
+    if(_title == undefined || _text == undefined){
+        res.redirect("/admin/articles/new");
+    }else{
+        Article.create({
+            title: _title,
+            slug: slugify(_title),
+            body: _text
+        }).then(() => {
+            res.redirect("/admin/articles/new");
+        })
+    }
 })
 
 module.exports = router;
