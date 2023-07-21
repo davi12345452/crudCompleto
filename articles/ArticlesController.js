e8ef40e0f615f0ee09a95373d2d17d569acb572a
Davi <88855441+davi12345452@users.noreply.github.com>
2023-07-21 18:12:39 -0300

    editor de artigos, quase pronto

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -23,12 +23,35 @@ router.get("/admin/articles/new", (req, res) => {
 // Rota para a página que lista os artigos
 
 router.get("/admin/articles", (req, res) => {
-    Article.findAll({raw:true}).then(_articles => {
+    Article.findAll({include: [{model: Category}]}).then(_articles => {
         res.render("admin/articles/index.ejs", {
-            articles: _articles
+            articles: _articles,          
         });
-    })
-    
+    })    
+})
+
+// Rota para editar artigo individualmente
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    let _id = req.params.id;
+    if(isNaN(_id)){
+        res.redirect("/admin/articles"); 
+    }
+    // É uma maneira do Sequelize, mais rápida, de encontra por id
+    Article.findByPk(_id).then(article => {
+        if(article != undefined){
+            Category.findAll({raw:true}).then((_categories) => {
+                res.render("admin/articles/edit.ejs", {artigo: article, categories: _categories});
+            })
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(() => res.redirect("/admin/articles"));
+})
+
+// Rota para exibir artigo individual
+router.get("/admin/articles/:id", (req, res) => {
+    res.render("admin/articles/view");
 })
 
 // Rota post para criar um artigo
@@ -47,9 +70,55 @@ router.post("/articles/new-save", (req, res) => {
             // Relacionamento das tabelas:
             categoryId: _category
         }).then(() => {
-            res.redirect("/admin/articles/new");
+            res.redirect("/admin/articles");
         })
     }
+})
+
+// Rota para deletar um artigo:
+
+router.post("/articles/delete", (req, res) => {
+    let _id = req.body.id;
+    if(_id != undefined){
+        if(!isNaN(_id)){
+            Article.destroy({
+                where:{
+                    id: _id
+                }
+            }).then( () => {
+                res.redirect("/admin/articles");
+            })
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }else{
+        res.redirect("/admin/articles");
+    }
+})
+
+// Rota para editar um artigos:
+
+router.post("/articles/edit", (req, res) => {
+    let _id = req.body.id;
+    let _title = req.body.title;
+    let _text = req.body.text;
+    let _category = req.body.category;
+    Article.update(
+        {
+        title: _title,
+        slug: slugify(_title),
+        body: _text,
+        categoryId: _category
+        }, 
+        {
+        where:{
+            id: _id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(() => {
+        res.redirect("/admin/articles");
+    })
 })
 
 module.exports = router;
--- database/db.js
+++ database/db.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize(process.env.NOME_DO_DB, process.env.USUARIO_MYSQL, process.env.SENHA_MYSQL, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
